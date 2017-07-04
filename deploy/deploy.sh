6b0292d336ffcf5c0a5820cca18c65e66b954799
Andriy S'omak <semack@users.noreply.github.com>
2017-07-05 01:42:38 +0300

    Delete deploy.sh

--- deploy/deploy.sh
+++
@@ -1,5 +0,0 @@
-ApiKey=$1
-Source=$2
-
-nuget pack ./deploy/AspNetCore.FriendlyExceptions.nuspec -Verbosity detailed
-nuget push ./AspNetCore.FriendlyExceptions.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
