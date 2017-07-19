51e8f42ece9d4e2a74b4eb6805350988196f0699
Andriy S'omak <semack@users.noreply.github.com>
2017-07-19 14:22:26 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+dotnet restore
+
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision) 
+
+dotnet pack --configuration release --output nupkgs --version-suffix=$revision  
+dotnet nuget push ./src/nupkgs/AspNetCore.FriendlyExceptions.*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE
