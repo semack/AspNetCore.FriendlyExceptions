162a62c34403deacfda823fbdf22b4f093637d7e
Andriy S'omak <semack@users.noreply.github.com>
2017-07-19 14:51:16 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ dotnet restore
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 
 
-dotnet pack --configuration release --output nupkgs --version-suffix $revision 
+dotnet pack --configuration release --output nupkgs --version-suffix 123 
 dotnet nuget push ./src/nupkgs/AspNetCore.FriendlyExceptions.*.nupkg --api-key $NUGET_API_KEY --source $NUGET_SOURCE
